30302b7f5b2d4ec8a523b4d7b62c088dfb2906c1
zhao3154075 <515280634@qq.com>
2018-10-11 14:57:01 +0800

    无

--- prize-recorder/src/main/java/com/es/prizerecorder/listener/PrizeListener.java
+++ prize-recorder/src/main/java/com/es/prizerecorder/listener/PrizeListener.java
@@ -6,6 +6,7 @@ import com.es.ssp.model.Fans;
 import com.es.ssp.model.PrizeRecord;
 import com.es.ssp.pojo.Redpack;
 import com.es.ssp.wechatapi.WechatApi;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,7 +43,7 @@ public class PrizeListener {
                 if(result==null||result.getReturn_code().equals("FAIL")){//如果结果为空或者请求失败直接返回
                     return;
                 }
-                if(result.getResult_code().equals("SUCCESS")){//如果该单号其实已经发送成功，直接修改发送记录的状态
+                if(result.getResult_code().equals("SUCCESS")&& StringUtils.isEmpty(result.getReason())){//如果该单号其实已经发送成功，直接修改发送记录的状态
                     prizeRecord.setStatus(1);
                     prizeRecord.setErrorInfo("");
                     prizeRecordManager.update(prizeRecord);
